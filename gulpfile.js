b4b249384ca64aebc6f076c565c2c352e1801435
Felipe Nuñez D <fnunez7@bancoestado.cl>
2020-03-09 17:43:16 -0300

    core(gulp):integrate gulp to build dist

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const { task, src, series, dest } = require('gulp');
+const clean = require('gulp-clean');
+const file = require('gulp-file');
+const fs = require('fs');
+const pkg = require('./package.json');
+
+
+task('cleanDist', async () => {
+    return src('./dist', { read: false, allowEmpty: true }).pipe(clean({ force: true }))
+})
+
+task('copyToDist', async () => {
+    return src('./src/module/**/*').pipe(dest('./dist/'))
+})
+
+task('makePackageJson', async () => {
+    let distPkg = {
+        dependencies: {
+            "athena-express": "^5.1.1"
+        },
+        peerDependencies: {
+            "aws-sdk": "^2.635.0"
+        }
+    }
+
+    distPkg.version = pkg.version;
+    distPkg.name = pkg.name;
+    distPkg.description = pkg.description;
+    distPkg.main = pkg.main;
+    distPkg.keywords = pkg.keywords;
+    distPkg.license = pkg.license;
+
+    return file('package.json', JSON.stringify(distPkg,null,2), { src: true })
+    .pipe(dest('dist'));
+
+
+})
+
+task('build', series(
+    'cleanDist',
+    'copyToDist',
+    'makePackageJson'
+)); 
