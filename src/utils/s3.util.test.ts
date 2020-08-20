128fd2ffd79b458f4b4c86335f920c7dab365fb4
Felipe Nuñez <toutox@gmail.com>
2020-08-20 17:26:04 -0400

    feat(s3Util):add fileDetail function

--- src/utils/s3.util.test.ts
+++ src/utils/s3.util.test.ts
@@ -1,38 +1,45 @@
-import { S3Utils } from './s3.util'
-import * as fs from 'fs'
-
+import { S3Utils } from "./s3.util";
+import * as fs from "fs";
 
 jest.setTimeout(100000);
-describe('S3 Util Test', () => {
+describe("S3 Util Test", () => {
+  let s3Util: S3Utils;
+  beforeAll(() => {
+    let config = { region: "us-east-1" };
+    s3Util = new S3Utils(config);
+  });
 
-    let s3Util: S3Utils;
-    beforeAll(() => {
-        let config = { region: 'us-east-1' }
-        s3Util = new S3Utils(config);
-    })
+  test("Must be get s3 element", () => {
+    let s3Url = `s3://bucket/folder1/foler2/folder3/file.txt`;
+    let element = S3Utils.urlDeconstruct(s3Url);
+    //console.log(element);
+  });
 
-    test('Must be get s3 element', () => {
-        let s3Url = `s3://bucket/folder1/foler2/folder3/file.txt`
-        let element = S3Utils.urlDeconstruct(s3Url);
-        //console.log(element);
-    })
+  test("get object", async () => {
+    let fileURL =
+      "s3://test-app-rules/querys/6abf3a38-9948-4df1-8d1d-34df3e60bd4b.csv";
+    let data = s3Util.getStream(fileURL);
+    //console.log(data);
+  });
 
-    test('get object', async () => {
-        let fileURL = 's3://test-app-rules/querys/6abf3a38-9948-4df1-8d1d-34df3e60bd4b.csv'
-        let data = s3Util.getStream(fileURL);
-        //console.log(data);
+  test("test uploadFile", async () => {
+    fs.mkdirSync(`./tmp`);
+    let file = fs.openSync(`./tmp/test.txt`, `w+`);
+    for (let i = 0; i < 100000; i++) {
+      fs.writeSync(file, Buffer.from(`line n° ${i + 1} from test file\n`));
+    }
+    fs.closeSync(file);
 
-    })
+    await s3Util.uploadFile(
+      `./tmp/test.txt`,
+      `test-app-rules`,
+      `upload-test/test.txt`
+    );
+    fs.rmdirSync(`./tmp`, { recursive: true });
+  });
 
-    test('test uploadFile', async () => {
-        fs.mkdirSync(`./tmp`)
-        let file = fs.openSync(`./tmp/test.txt`, `w+`);
-        for (let i = 0; i < 100000; i++) {
-            fs.writeSync(file, Buffer.from(`line n° ${i + 1} from test file\n`))
-        }
-        fs.closeSync(file);
-
-        await s3Util.uploadFile(`./tmp/test.txt`, `test-app-rules`, `upload-test/test.txt`)
-        fs.rmdirSync(`./tmp`, { recursive: true })
-    })
+  test.only("test file Detail", async ()=>{
+      const {bucket,key} = {bucket:"test-app-rules", key:"afp.py"}
+      let result = await s3Util.getFileDetail(bucket,key)
+  });
 });
--- src/utils/s3.util.ts
+++ src/utils/s3.util.ts
@@ -1,150 +1,168 @@
-import { getAWSSdk } from '../service/sdk.service'
-import { S3Element } from '../domain/S3Element'
-import * as AWS from 'aws-sdk';
-import { API_VERSION } from '../config/apiversion'
-import { readFileSync } from 'fs'
+import { getAWSSdk } from "../service/sdk.service";
+import { S3Element } from "../domain/S3Element";
+import * as AWS from "aws-sdk";
+import { API_VERSION } from "../config/apiversion";
+import { readFileSync } from "fs";
 
 export class S3Utils {
-
-    private s3Api: AWS.S3;
-    constructor(config = <any>undefined) {
-        let aws = getAWSSdk();
-        if (!config) {
-            if (aws.configured) {
-                this.s3Api = new aws.sdk.S3({ apiVersion: API_VERSION.athena });
-            } else {
-                throw new Error("No config params, create a toryas.config.js file in the root or send a config object in constructor");
-            }
-        } else {
-            AWS.config.update(config);
-            this.s3Api = new AWS.S3({ apiVersion: API_VERSION.athena });
-        }
+  private s3Api: AWS.S3;
+  constructor(config = <any>undefined) {
+    let aws = getAWSSdk();
+    if (!config) {
+      if (aws.configured) {
+        this.s3Api = new aws.sdk.S3({ apiVersion: API_VERSION.athena });
+      } else {
+        throw new Error(
+          "No config params, create a toryas.config.js file in the root or send a config object in constructor"
+        );
+      }
+    } else {
+      AWS.config.update(config);
+      this.s3Api = new AWS.S3({ apiVersion: API_VERSION.athena });
     }
+  }
 
-    /**
-     * Get S3 Object as steream
-     * 
-     * @param fileURL S3 Object's URL  example: s3://myBucket/folder/file.txt
-     */
-    public getStream(fileURL: string): any {
-        let s3File = S3Utils.urlDeconstruct(fileURL);
-        let s3Object = this.s3Api.getObject({ Bucket: s3File.bucket, Key: s3File.key })
-        return s3Object.createReadStream();
+  /**
+   * Get S3 Object as steream
+   *
+   * @param fileURL S3 Object's URL  example: s3://myBucket/folder/file.txt
+   */
+  public getStream(fileURL: string): any {
+    let s3File = S3Utils.urlDeconstruct(fileURL);
+    let s3Object = this.s3Api.getObject({
+      Bucket: s3File.bucket,
+      Key: s3File.key,
+    });
+    return s3Object.createReadStream();
+  }
+
+  /**
+   * Copy a S3 Object to new destination
+   *
+   * @param {string} fileURL S3 Object's URL  example: s3://myBucket/folder/file.txt
+   * @param {string} newBucket  destination bucket EJ: myNewBucket
+   * @param {string} newKey  S3 Key destination EJ: otherFolder/file.txt
+   * @return {Promise<string>} return S3 Object's URL destination if process success.
+   */
+  public async copyFile(
+    fileURL: string,
+    newBucket: string,
+    newKey: string
+  ): Promise<string> {
+    try {
+      let s3Elements = S3Utils.urlDeconstruct(fileURL);
+      let paramsCopy = {
+        Bucket: newBucket,
+        CopySource: `${s3Elements.bucket}/${s3Elements.key}`,
+        Key: newKey,
+      };
+      let resp = await this.s3Api.copyObject(paramsCopy).promise();
+      return `s3://${newBucket}/${newKey}`;
+    } catch (e) {
+      throw new TypeError("Error on copy file");
     }
+  }
 
-    /**
-     * Copy a S3 Object to new destination
-     * 
-     * @param {string} fileURL S3 Object's URL  example: s3://myBucket/folder/file.txt
-     * @param {string} newBucket  destination bucket EJ: myNewBucket
-     * @param {string} newKey  S3 Key destination EJ: otherFolder/file.txt
-     * @return {Promise<string>} return S3 Object's URL destination if process success.
-     */
-    public async copyFile(fileURL: string, newBucket: string, newKey: string): Promise<string> {
-        try {
-            let s3Elements = S3Utils.urlDeconstruct(fileURL);
-            let paramsCopy = {
-                Bucket: newBucket,
-                CopySource: `${s3Elements.bucket}/${s3Elements.key}`,
-                Key: newKey
-            }
-            let resp = await this.s3Api.copyObject(paramsCopy).promise();
-            return `s3://${newBucket}/${newKey}`;
-        } catch (e) {
-            throw new TypeError("Error on copy file")
-        }
+  /**
+   * Decontruct S3 Object's URL
+   * @param {String} s3URL S3 Object's URL
+   *
+   * @returns {S3Element}
+   *
+   * @example
+   *
+   * let url = `s3://myBucket/folder1/folder2/file.txt`
+   * let s3Element = urlDeconstruct(url);
+   * console.log(s3Element)
+   * //S3Element {
+   * //  bucket:"myBucket"
+   * //  key:"folder1/folder2/file.txt"
+   * //  path:"folder1/folder2/"
+   * //  file:"file.txt"
+   * //}
+   *
+   */
+  static urlDeconstruct(S3Url: string): S3Element {
+    let parts = S3Url.split("/");
+    return new S3Element(parts[2], parts.slice(3).join("/"));
+  }
+
+  /**
+   * Rename Object in S3
+   * @param {String} fileURL file's path, example : s3://myBucket/folder/file.txt
+   * @param {String} newName file's new name, example: file.txt
+   */
+  public async renameFile(fileURL: string, newName: any) {
+    try {
+      let s3Elements = S3Utils.urlDeconstruct(fileURL);
+      let copyResult = await this.copyFile(
+        fileURL,
+        s3Elements.bucket,
+        `${s3Elements.path}${newName}`
+      );
+      if (copyResult) {
+        await this.deleteFile(s3Elements.bucket, s3Elements.key);
+      }
+      return copyResult;
+    } catch (e) {
+      throw new TypeError("Error trying rename file");
     }
+  }
 
-    /**
-     * Decontruct S3 Object's URL 
-     * @param {String} s3URL S3 Object's URL
-     * 
-     * @returns {S3Element}
-     * 
-     * @example
-     * 
-     * let url = `s3://myBucket/folder1/folder2/file.txt`
-     * let s3Element = urlDeconstruct(url);
-     * console.log(s3Element)
-     * //S3Element {
-     * //  bucket:"myBucket"
-     * //  key:"folder1/folder2/file.txt"
-     * //  path:"folder1/folder2/"
-     * //  file:"file.txt"
-     * //}
-     * 
-     */
-    static urlDeconstruct(S3Url: string): S3Element {
-        let parts = S3Url.split('/');
-        return new S3Element(parts[2], parts.slice(3).join('/'));
+  /**
+   * Delete a s3 Object
+   * @param {string} bucket  S3 Bucket's name example: myBucket
+   * @param {string} key  S3 Object's key example: path/file.txt
+   * @returns {Promise<boolean>} if delete are done "true" else "false"
+   */
+  public async deleteFile(bucket: string, key: string): Promise<boolean> {
+    try {
+      let deleteParameters = {
+        Bucket: bucket,
+        Key: key,
+      };
+      await this.s3Api.deleteObject(deleteParameters).promise();
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 
-    /**
-     * Rename Object in S3
-     * @param {String} fileURL file's path, example : s3://myBucket/folder/file.txt
-     * @param {String} newName file's new name, example: file.txt
-     */
-    public async renameFile(fileURL: string, newName: any) {
-        try {
-            let s3Elements = S3Utils.urlDeconstruct(fileURL);
-            let copyResult = await this.copyFile(fileURL, s3Elements.bucket, `${s3Elements.path}${newName}`);
-            if (copyResult) {
-                await this.deleteFile(s3Elements.bucket, s3Elements.key);
-            }
-            return copyResult;
-        } catch (e) {
-            throw new TypeError("Error trying rename file")
-        }
+  /**
+   * Upload file to S3
+   *
+   * @param filePath file path
+   * @param bucket bucket name
+   * @param key s3 key for file - example : path/path/file.txt
+   */
+  public async uploadFile(filePath: string, bucket: string, key: string) {
+    let fileContent = readFileSync(filePath);
+    let options = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+    let params = {
+      Body: fileContent,
+      Bucket: bucket,
+      Key: key,
+    };
+
+    return await this.s3Api.upload(params, options).promise();
+  }
+
+  /**
+   * Return 
+   * @param bucket S3 bucket
+   * @param key S3 key
+   */
+  public async getFileDetail(bucket: string, key: string) {
+    let params = {
+      Bucket: bucket,
+      Key: key,
+    };
+    let resp =  await this.s3Api.headObject(params).promise();
+    return {
+        fileSize:resp.ContentLength,
+        fileSizeUnit: resp.AcceptRanges,
+        ETag:resp.ETag,
+        contentType:resp.ContentType
     }
-
-
-    /**
-     * Delete a s3 Object
-     * @param {string} bucket  S3 Bucket's name example: myBucket
-     * @param {string} key  S3 Object's key example: path/file.txt
-     * @returns {Promise<boolean>} if delete are done "true" else "false"
-     */
-    public async deleteFile(bucket: string, key: string): Promise<boolean> {
-        try {
-            let deleteParameters = {
-                Bucket: bucket,
-                Key: key
-            }
-            await this.s3Api.deleteObject(deleteParameters).promise();
-            return true;
-        } catch (e) {
-            return false;
-        }
-    }
-
-    /**
-     * Upload file to S3
-     * 
-     * @param filePath file path
-     * @param bucket bucket name
-     * @param key s3 key for file - example : path/path/file.txt
-     */
-    public async uploadFile(filePath: string, bucket: string, key: string) {
-
-        let fileContent = readFileSync(filePath)
-        let options = {partSize: 10 * 1024 * 1024, queueSize: 1};
-        let params = {
-            Body: fileContent,
-            Bucket: bucket,
-            Key: key
-        }
-
-        return await this.s3Api.upload(params,options).promise();
-
-    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
